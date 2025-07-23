ce41b8dbc3426fecdb6fa43e41b583dfe36bcb24
eduardotecnologo <eduardotecnologo@hotmail.com>
2025-07-23 19:58:09 -0300

    feat: create project

---
+++ MServices/Delivery-Trackin/src/main/java/com/edudeveloper/delivery/Delivery/Trackin/DeliveryTrackinApplication.java
@@ -0,0 +1,13 @@
+package com.edudeveloper.delivery.Delivery.Trackin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTrackinApplication.class, args);
+	}
+
+}
---
+++ MServices/Delivery-Trackin/src/test/java/com/edudeveloper/delivery/Delivery/Trackin/DeliveryTrackinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudeveloper.delivery.Delivery.Trackin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
